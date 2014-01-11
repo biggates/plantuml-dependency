0db531454f77c1123582d18c00ef9e61c34b105a
graffity2199 <graffity2199@8eed4c46-011d-4db3-ab61-caef2b46e43d>
2014-01-11 13:13:36 +0000

    Changing package name

---
+++ src/plantuml-dependency-cli/src/test/resources/net/sourceforge/plantumldependency/cli/main/option/output/b/Annotationinterface.java
@@ -0,0 +1,36 @@
+/*
+ Annotationinterface.java
+ Creation date : 2 juil. 2013
+ Copyright © Benjamin Croizet (graffity2199@yahoo.fr)
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ or GNU Lesser General Public License as published by the
+ Free Software Foundation; either version 3 of the License,
+ or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received copies of the GNU General Public License
+ and GNU Lesser General Public License along with this program;
+ if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ http://www.fsf.org/licensing/licenses/gpl.html
+ http://www.gnu.org/licenses/lgpl.html
+ */
+package net.sourceforge.plantumldependency.main.option.output;
+
+import java.lang.annotation.ElementType;
+
+/**
+ * @author Benjamin Croizet (<a href="mailto:graffity2199@yahoo.fr>graffity2199@yahoo.fr</a>)
+ *
+ * @since 1.0
+ * @version
+ */
+public @interface Annotationinterface {
+
+}
