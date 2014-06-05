bb5d523542380ed1b7c599a38e4e7e3fb6983c9b
graffity2199 <graffity2199@8eed4c46-011d-4db3-ab61-caef2b46e43d>
2014-06-05 20:19:57 +0000

    1.4.0 Release

--- src/plantuml-dependency-cli/src/main/java/net/sourceforge/plantumldependency/cli/main/program/generated/PlantUMLDependencyProgramVersionImpl.java
+++ src/plantuml-dependency-cli/src/main/java/net/sourceforge/plantumldependency/cli/main/program/generated/PlantUMLDependencyProgramVersionImpl.java
@@ -36,5 +36,5 @@ public class PlantUMLDependencyProgramVersionImpl {
 
     public static final String PROGRAM_VERSION = "1.4.0-SNAPSHOT";
 
-    public static final String PROGRAM_BUILD_TIME = "20140531-1800";
+    public static final String PROGRAM_BUILD_TIME = "20140604-2231";
 }
